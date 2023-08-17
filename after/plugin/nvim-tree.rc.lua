7ca748e19b2278f5107376671042a821bb65237d
campbells <tranbaolam26902@gmail.com>
2023-08-17 15:00:43 +0700

    enable git

--- after/plugin/nvim-tree.rc.lua
+++ after/plugin/nvim-tree.rc.lua
@@ -4,7 +4,11 @@ if (not status) then return end
 vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
 
-nvim_tree.setup()
+nvim_tree.setup({
+    git = {
+        enable = true
+    }
+})
 
 local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', opts)
