8862e12b7df973fd6b089bc484ed2c08101ea901
campbells <tranbaolam26902@gmail.com>
2023-09-14 18:37:52 +0700

    disable statusline in nvim-tree

--- after/plugin/lualine.rc.lua
+++ after/plugin/lualine.rc.lua
@@ -7,7 +7,7 @@ lualine.setup {
         theme = 'auto',
         section_separators = { left = '', right = ''},
         component_separators = { left = '|', right = '|'},
-        disabled_filetypes = {}
+        disabled_filetypes = { 'NvimTree' }
     },
     secitons = {
         lualine_a = { 'mode' },
