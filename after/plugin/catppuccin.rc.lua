c51bcabfca2f8805bf6cad6d5cd7b9e4cd943778
campbells <tranbaolam26902@gmail.com>
2023-09-14 18:37:34 +0700

    force no italic

--- after/plugin/catppuccin.rc.lua
+++ after/plugin/catppuccin.rc.lua
@@ -1,9 +1,5 @@
 require("catppuccin").setup({
-    flavour = "mocha", -- latte, frappe, macchiato, mocha
-    background = { -- :h background
-        light = "latte",
-        dark = "mocha",
-    },
+    flavour = "frappe", -- latte, frappe, macchiato, mocha
     transparent_background = true, -- disables setting the background color.
     show_end_of_buffer = true, -- shows the '~' characters after the end of buffers
     term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
@@ -12,25 +8,9 @@ require("catppuccin").setup({
         shade = "dark",
         percentage = 0.15, -- percentage of the shade to apply to the inactive window
     },
-    no_italic = false, -- Force no italic
+    no_italic = true, -- Force no italic
     no_bold = false, -- Force no bold
     no_underline = false, -- Force no underline
-    styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-        comments = { "italic" }, -- Change the style of comments
-        conditionals = { "italic" },
-        loops = {},
-        functions = {},
-        keywords = {},
-        strings = {},
-        variables = {},
-        numbers = {},
-        booleans = {},
-        properties = {},
-        types = {},
-        operators = {},
-    },
-    color_overrides = {},
-    custom_highlights = {},
     integrations = {
         cmp = true,
         gitsigns = true,
